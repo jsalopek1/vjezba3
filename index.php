98941e607c059a251c9d5f5061ff218963736568
jsalopek1 <jsalopek@tvz.hr>
2024-10-21 18:16:14 +0200

    Vjezba 3

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+// Konkatenacija
+$title = "<p> Da Vincijev";
+$title .= " kod</p>"; 
+$link = "<p>https://hr.wikipedia.org/";
+$link .= "Da_Vincijev_kod</p>"; 
+?>
+
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Davincije kod">
+    <meta name="keywords" content="Da Vincijev kod, Dan Brown, kriminalistički triler">
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+    <h1><?php echo $title; ?></h1>
+    <p>Da Vincijev kod je kriminalistički triler američkog pisca Dana Browna.</p>
+
+    <p>
+        <a href="<?php echo $link; ?>" target="_blank">Link na wikipediju</a>
+    </p>
+
+    <!-- vjezba 3 - Davinecijev kod -->
+</body>
+</html>
